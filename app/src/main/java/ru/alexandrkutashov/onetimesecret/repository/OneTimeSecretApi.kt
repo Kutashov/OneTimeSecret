f063cff06e2f9b7b5fa8eb520b1555024e800260
Alexandr Kutashov <kutashov.alexandr@yandex.ru>
2018-02-25 19:04:59 +0300

    Move api interface to implementation

--- app/src/main/java/ru/alexandrkutashov/onetimesecret/repository/OneTimeSecretApi.kt
+++ app/src/main/java/ru/alexandrkutashov/onetimesecret/repository/OneTimeSecretApi.kt
@@ -14,6 +14,42 @@ import ru.alexandrkutashov.onetimesecret.repository.model.*
  *         on 23.02.2018
  */
 
+interface OneTimeSecret {
+
+    /**
+     * Current status of the system.
+     */
+    fun status(): Pair<StatusResponse?, Exception?>
+
+    /**
+     * Stores a secret value.
+     */
+    fun share(shareRequest: ShareRequest): Pair<ShareResponse?, Exception?>
+
+    /**
+     * Generate a short, unique secret. This is useful for temporary passwords, one-time pads, salts, etc.
+     */
+    fun generate(generateRequest: GenerateRequest): Pair<GenerateResponse?, Exception?>
+
+    /**
+     * Retrieve a generated secret.
+     */
+    fun retrieve(retrieveRequest: RetrieveRequest): Pair<RetrieveResponse?, Exception?>
+
+    /**
+     * Every secret also has associated metadata. The metadata is intended to be used by the creator of the secret
+     * (i.e. not the recipient) and should generally be kept private. You can safely use the metadata key to retrieve
+     * basic information about the secret itself (e.g. if or when it was viewed) since the metadata key is different
+     * from the secret key.
+     */
+    fun metadata(metadataRequest: MetadataRequest): Pair<MetadataResponse?, Exception?>
+
+    /**
+     * Burn a secret that has not been read yet.
+     */
+    fun burn(burnRequest: BurnRequest): Pair<BurnResponse?, Exception?>
+}
+
 class OneTimeSecretImpl : OneTimeSecret {
 
     val TAG = OneTimeSecretImpl::class.simpleName
